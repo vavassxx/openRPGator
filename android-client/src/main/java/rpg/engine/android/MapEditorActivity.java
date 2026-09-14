a0f73529ae6aea080065f0e5c5c352bc985305d3
vavass <vavass@internet.ru>
2026-09-14 16:19:52 +0300

    Some fixes of import

--- android-client/src/main/java/rpg/engine/android/MapEditorActivity.java
+++ android-client/src/main/java/rpg/engine/android/MapEditorActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.*;
 import android.net.Uri;
-import android.os.Bundle;
+import android.os.*;
 import android.view.*;
 import android.widget.*;
 import java.io.*;
